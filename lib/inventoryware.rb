b59056cfb69d5e37fe19a12d18ef577437043f39
DavidMarchant <davidwjmarchant@gmail.com>
2018-11-29 11:11:23 +0000

    Implement searching of lshw
    In order to use lshw library a (tmp) file for lshw.xml must be created.
    
    As the file destination for ZipFile.extract must be empty, a tempdir
    is created and then a hard path is set for lshw.xml within there.
    
    The use of the lshw library is unsatisfactory as much functionality
    is missing & must be monkeypatched in, however it will have to do for
    now due to time constraints. Expect this methodology to be overhauled.
    
    Additionally all code has been moved into lib

--- lib/inventoryware.rb
+++ lib/inventoryware.rb
@@ -22,7 +22,7 @@
 #==============================================================================
 
 lib_dir = File.dirname(__FILE__)
-ENV['BUNDLE_GEMFILE'] ||= File.join(lib_dir, 'Gemfile')
+ENV['BUNDLE_GEMFILE'] ||= File.join(lib_dir, '..', 'Gemfile')
 
 require 'rubygems'
 require 'bundler'
@@ -34,18 +34,26 @@ else
   Bundler.setup(:default)
 end
 
-require './cli'
+require_relative 'cli'
+require_relative 'extensions/lshw'
 require 'zip'
+require 'tmpdir'
 
 def check_data_source?(data_source)
   !(File.file?(data_source) && File.extname(data_source) == ".zip")
 end
 
 begin
+  dir = Dir.mktmpdir('inv_ware_')
+  tmp_lshw_xml = File.join(dir, 'lshw_xml')
+
   # Parse arguments
   options = MainParser.parse(ARGV)
 
   data_source = options['data_source']
+  node = options['node']
+  hash = {}
+  hash[node] = {}
 
   if check_data_source?(data_source)
     puts "Error with data source #{data_source}"\
@@ -57,7 +65,38 @@ begin
     zip_file.each do |entry|
       puts "Extracting #{entry.name}"
     end
-    lshw_xml_file = zip_file.glob('lshw-xml').first
-    lsbik_file = zip_file.glob('lsbik').first
+    zip_file.glob('lshw-xml.txt').first.extract(tmp_lshw_xml)
+    #lsbik_file = zip_file.glob('lsbik.txt').first
   end
+
+  #TODO sort error conditions here
+  f = File.open(tmp_lshw_xml)
+  lshw = Lshw::XML(f)
+  f.close
+
+  hash[node]['Hardware Type'] = lshw.product
+
+  hash[node]['System Serial Number'] = lshw.serial
+
+  hash[node]['cpus'] = {}
+  lshw.cpus.each do |cpu|
+    hash[node]['cpus'][cpu.id] = {}
+    hash[node]['cpus'][cpu.id]['product'] = cpu.product
+    hash[node]['cpus'][cpu.id]['slot'] = cpu.slot
+  end
+
+  total_memory = 0
+  lshw.all_memory.each do |mem|
+    mem.banks.each do |bank|
+      total_memory += bank.size
+    end
+  end
+  hash[node]['Total Memory'] = total_memory
+
+  lshw.all_networks.each do |net|
+    hash[node][net.logical_name] = {"serial"=>net.mac}
+  end
+  puts hash
+ensure
+  FileUtils.remove_entry dir
 end
