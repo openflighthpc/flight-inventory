67a837c21d1bb23ea201ae3825892947e8d81da0
DavidMarchant <davidwjmarchant@gmail.com>
2018-12-03 17:06:53 +0000

    Add licensing for LsblkParser

--- lib/lsblk_parser.rb
+++ lib/lsblk_parser.rb
@@ -1,3 +1,25 @@
+#==============================================================================
+# Copyright (C) 2018 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces inventoryware.
+#
+# Alces inventoryware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces inventoryware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on Alces inventoryware, please visit:
+# https://github.com/alces-software/inventoryware
+#==============================================================================
+
 # Only designed on lsblk output with the -a (all) and 
 # -P (key/value pairs) options
 class LsblkParser
