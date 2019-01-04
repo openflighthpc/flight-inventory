711ebe1a899e2a7392e3fa42cd036f836aceabe5
DavidMarchant <davidwjmarchant@gmail.com>
2019-01-04 16:07:59 +0000

    Rework example plugins dir for new hash names
    Mainly swapping identifiers

--- plugins/cpu.rb
+++ plugins/cpu.rb
@@ -1,11 +1,11 @@
-def cpus(hash)
+def cpus
   def create_cpu(cpu_hash)
     OpenStruct.new(cpu_hash).tap do |o|
       o.model = cpu_hash['model'] || cpu_hash['version'] || 'No model found'
     end
   end
   cpus = []
-  find_hashes_with_key_value(hash, 'class', 'processor').each do |cpu_hash|
+  find_hashes_with_key_value(@node_data, 'class', 'processor').each do |cpu_hash|
     cpus << create_cpu(cpu_hash)
   end
   cpus
