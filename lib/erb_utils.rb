6fc33d84904ac5a431dd8c980f39c15953522870
DavidMarchant <davidwjmarchant@gmail.com>
2019-01-16 16:19:22 +0000

    Add a license to each file
    Also fix capitalisation and dates for existing ones

--- lib/erb_utils.rb
+++ lib/erb_utils.rb
@@ -1,3 +1,25 @@
+#==============================================================================
+# Copyright (C) 2018-19 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Inventoryware.
+#
+# Alces Inventoryware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Inventoryware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on Alces Inventoryware, please visit:
+# https://github.com/alces-software/inventoryware
+#==============================================================================
+
 #value' can be a regular expression or a plain old string
 def find_hashes_with_key_value(obj, key, value, store = [])
   if obj.respond_to?(:key?) && obj.key?(key) && /#{value}/.match(obj[key])
--- lib/inventoryware.rb
+++ lib/inventoryware.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 #==============================================================================
-# Copyright (C) 2018 Stephen F. Norledge and Alces Software Ltd.
+# Copyright (C) 2018-19 Stephen F. Norledge and Alces Software Ltd.
 #
-# This file/package is part of Alces inventoryware.
+# This file/package is part of Alces Inventoryware.
 #
-# Alces inventoryware is free software: you can redistribute it and/or
+# Alces Inventoryware is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Affero General Public License
 # as published by the Free Software Foundation, either version 3 of
 # the License, or (at your option) any later version.
 #
-# Alces inventoryware is distributed in the hope that it will be useful,
+# Alces Inventoryware is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Affero General Public License for more details.
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this package.  If not, see <http://www.gnu.org/licenses/>.
 #
-# For more information on Alces inventoryware, please visit:
+# For more information on Alces Inventoryware, please visit:
 # https://github.com/alces-software/inventoryware
 #==============================================================================
 
