ff0b864a681bfdff4852f5859c2a1d7dd63bb83c
stuts <stu.j.franks@gmail.com>
2019-01-30 15:46:45 +0000

    Use openstruct, rescue to empty, fix typo

--- plugins/helpers/bios_selector.rb
+++ plugins/helpers/bios_selector.rb
@@ -1,11 +1,11 @@
 def select_bios
-  product = @node_data['lshw']['list']['node']['product']
+  product = @node_data.lshw.list.node.product rescue ''
   product.downcase!
   product = product.split(' ')
 
   # we look for sub templates with increasingly short names
   #   we start with words 1 to n, then 1 to n-1 etc.
-  # this function expects snake case tempaltes stored in a
+  # this function expects snake case templates stored in a
   #   `templates/bios/` directory.
   # It is possible that all but the first line of this method should be moved
   #   to `erb_utils` as it will be repeated for all sub-template rendering but
