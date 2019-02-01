3a8712ae92aad8029e31a3ea2d49b195b08b5ffb
DavidMarchant <davidwjmarchant@gmail.com>
2019-02-01 16:41:48 +0000

    Create superclass for commands that select >1 node

---
+++ lib/commands/multi_node_command.rb
@@ -0,0 +1,37 @@
+#==============================================================================
+# Copyright (C) 2018-19 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Inventoryware.
+#
+# Alces Inventoryware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Inventoryware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on Alces Inventoryware, please visit:
+# https://github.com/alces-software/inventoryware
+#==============================================================================
+
+module Inventoryware
+  module Commands
+    class MultiNodeCommand < Command
+      def find_nodes(return_missing, *other_args)
+        Utils::resolve_node_options(@argv, @options, other_args)
+
+        nodes = @argv.dig(other_args.length)
+
+        node_locations = Utils::select_nodes(nodes,
+                                             @options,
+                                             return_missing)
+      end
+    end
+  end
+end
