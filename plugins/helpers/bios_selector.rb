bfa53616f67004c6606ccf900656851246ce3f65
DavidMarchant <davidwjmarchant@gmail.com>
2019-02-04 12:20:57 +0000

    Fix comment
    Inacurrately asking for snake case templates when in actuality
    hyphenated names are required (aka kebab case)

--- plugins/helpers/bios_selector.rb
+++ plugins/helpers/bios_selector.rb
@@ -4,7 +4,7 @@ def select_bios
 
   # we look for sub templates with increasingly short names
   #   we start with words 1 to n, then 1 to n-1 etc.
-  # this function expects snake case templates stored in a
+  # this function expects kebab-case-templates stored in a
   #   `templates/bios/` directory.
   # It is possible that all but the first line of this method should be moved
   #   to `erb_utils` as it will be repeated for all sub-template rendering but
