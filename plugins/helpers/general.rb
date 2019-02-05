c0bd3e8162e5cac6e398a20c94570fc69fbeda46
DavidMarchant <davidwjmarchant@gmail.com>
2019-02-05 17:26:29 +0000

    Fix helpers
    As they require teh use of node_hash not node_data to manipulate the
    data

--- plugins/helpers/general.rb
+++ plugins/helpers/general.rb
@@ -1,7 +1,7 @@
 # sum the size of each bank of each memory node
 def find_total_memory
   total = 0
-  find_hashes_with_key_value(@node_data, 'class', 'memory').each do |mem|
+  find_hashes_with_key_value(@node_hash, 'class', 'memory').each do |mem|
     find_hashes_with_key_value(mem, 'id', '^(bank:).*').each do |bank|
       total += bank['size'].to_i
     end
