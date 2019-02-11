2f0fb725bd8a7899599b0bb88d52d48a17c42772
DavidMarchant <davidwjmarchant@gmail.com>
2019-02-11 11:40:07 +0000

    Split `erb_utils.rb` into seperate helpers

---
+++ helpers/templaters.rb
@@ -0,0 +1,31 @@
+#==============================================================================
+# Copyright (C) 2018-19 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Inventoryware.
+#
+# Alces Inventoryware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Inventoryware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on Alces Inventoryware, please visit:
+# https://github.com/alces-software/inventoryware
+#==============================================================================
+
+# all subtemplates are to be held in a specific directory of `templates/`
+def render_sub_template(subdir, name)
+  paths = Dir.glob(File.join(TEMPLATES_DIR, subdir, "#{name}.*"))
+  if paths.empty?
+    return false
+  else
+    return Erubis::Eruby.new(File.read(paths[0])).result(binding)
+  end
+end
