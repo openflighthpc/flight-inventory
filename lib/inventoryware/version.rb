3762ca089e0866d7097a6e1e66dc316f4cdd0077
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2019-04-18 15:53:13 +0100

    Bump version to 1.2.1 :)

--- lib/inventoryware/version.rb
+++ lib/inventoryware/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight-inventory
 # ==============================================================================
 module Inventoryware
-  VERSION = '1.0.0'
+  VERSION = '1.2.1'
 end
