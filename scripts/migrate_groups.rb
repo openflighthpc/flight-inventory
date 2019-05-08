84a774f4b742dfd55f8d856d16e7fa58051ec1c8
Andrew <andrew.toghill@alces-software.com>
2019-05-08 13:20:05 +0100

    Add script for migrating existing groups
    This migrates any existing groups to the new format allowing multiple
    maps on a single asset.

---
+++ scripts/migrate_groups.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+# =============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of Flight Inventory.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# Flight Inventory is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Inventory. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on Flight Inventory, please visit:
+# https://github.com/openflighthpc/flight-inventory
+# ==============================================================================
+
+def migrate_existing_group(asset)
+  data = asset.data
+  name = data['name']
+  mutable = data['mutable']
+
+  if mutable['map']
+    maps = mutable.fetch('maps') { mutable['maps'] = {} }
+    migrated_map = {}
+
+    ['map_height', 'map_width', 'map_layout', 'map'].each do |key|
+      migrated_map[key] = mutable[key]
+      mutable.delete(key)
+    end
+
+    maps['migrated'] = migrated_map
+    puts "Existing group migrated for asset: #{name}" if asset.save
+  end
+end
