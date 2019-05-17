50099ae62ebbde072e357c4cc8501fae7c3c05eb
Andrew <andrew.toghill@alces-software.com>
2019-05-17 11:38:17 +0100

    Adjust version for release

--- lib/inventoryware/version.rb
+++ lib/inventoryware/version.rb
@@ -25,7 +25,7 @@
 # https://github.com/openflighthpc/flight-inventory
 # ==============================================================================
 module Inventoryware
-  VERSION = '1.3.0-dev'
+  VERSION = '1.3.0'
   SCHEMA_NUM = 3
   REQ_SCHEMA_NUM = 3
 end
