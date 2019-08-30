711893ae691c1269b85305cb8286d0818e2e8160
VoxSecundus <millard64@hotmail.co.uk>
2019-08-30 15:20:11 +0100

    Changed import-hunter to convert from bytestring to zip, then invoking regular import

--- lib/inventoryware/commands/import_hunter.rb
+++ lib/inventoryware/commands/import_hunter.rb
@@ -67,22 +67,165 @@ Zip file at #{file_path} inaccessible.
             ERROR
           end
         end
-        nodelist = Utils.load_yaml(file_path)
-        nodelist.each do |key,value|
-          node_name = value["hostname"]
-          outyaml = YAML.load(value["payload"])
-          outyaml["name"] = node_name
-          node_data = outyaml
 
-          Utils.exit_unless_dir(Config.yaml_dir)
-          yaml_out_name = "#{node_name}.yaml"
-          out_file = File.join(Config.yaml_dir, yaml_out_name)
+        begin
+          top_dir = Dir.mktmpdir('inv_ware_')
+          top_dir2 = Dir.mktmpdir('inv_ware_2')
+          nodelist = Utils.load_yaml(file_path)
+          nodelist.each do |key,value|            
+            # convert payload to zip file, put in /tmp
+            payload_to_zip(value,top_dir)
+          end
 
-          node = Node.new(out_file)
-          node.data = node_data
-          node.save
+          # get all zips in in the source, if it's a dir or not
+          top_lvl_zip_paths = expand_dir(top_dir)
 
-          puts "#{node_name} imported to #{File.expand_path(out_file)}"
+          # for each of these, extract to /tmp/
+          top_lvl_zip_paths.each { |zip_path| extract_zip(zip_path, top_dir2) }
+
+          # extract any zips in these zips
+          recursively_extract_zips(top_dir2)
+
+          # remove empty file paths from the tmp dir
+          clean_dir(top_dir2)
+
+          # parse the extracted files to yaml
+          process_container_dir(top_dir2)
+        ensure
+          FileUtils.remove_entry top_dir
+          FileUtils.remove_entry top_dir2
+        end
+      end
+
+      private
+      def payload_to_zip(node,dir)
+        contents = node["payload"]
+        file_path=File.join(dir,"#{node["hostname"]}")
+        File.open(file_path, 'w+') do |f|
+          f.write(contents)
+        end
+        system("LC_ALL=C tr -cd 0-9a-fA-F < #{file_path} | xxd -r -p > #{file_path}.zip")
+      end
+
+      def expand_dir(data_source)
+        contents = []
+        if File.directory?(data_source)
+          contents = Dir.glob(File.join(data_source, "**/*.zip"))
+        elsif Utils.check_zip_exists?(data_source)
+          contents = [data_source]
+        end
+        if contents.empty?
+          raise ArgumentError, <<-ERROR.chomp
+No .zip files found at #{data_source}
+          ERROR
+        end
+        return contents
+      end
+
+      def recursively_extract_zips(target_dir)
+        things_changed = false
+        Dir[File.join(target_dir, '**/*.zip')].each do |zip_path|
+          extract_zip(zip_path, target_dir)
+          File.delete(zip_path)
+          things_changed = true
+        end
+        recursively_extract_zips(target_dir) if things_changed
+      end
+
+      def extract_zip(zip_path, destination)
+        Zip::File.open(zip_path) do |zip_file|
+          zip_name = File.basename(zip_file.name, '.zip')
+          zip_file.each do |item|
+            item_path = File.join(destination, zip_name, item.name)
+            FileUtils.mkdir_p(File.dirname(item_path))
+            zip_file.extract(item, item_path) unless File.exist?(item_path)
+          end
+        end
+      end
+
+      def clean_dir(dir)
+        # A bit of a hack to delete all directories with no files in them.
+        # By globbing then reversing the order you get a reversed-BFS.
+        # So all directories' items will preceded the directories themselves
+        # while being processed, allowing sequential deletion of empty dirs.
+        dir_items = Dir.glob(File.join(dir, "**/*"))
+        dir_items.reverse!
+        dir_items.each do |item|
+          if File.directory?(item) and Dir.empty?(item)
+            FileUtils.remove_dir(item)
+          end
+        end
+      end
+
+      def process_container_dir(dir)
+        Dir.foreach(dir) do |item|
+          next if item == '.' or item == '..'
+          if File.directory?(File.join(dir, item))
+            process_dir(File.join(dir, item))
+          end
+        end
+      end
+
+      def process_dir(dir)
+        node_name = File.basename(dir)
+        puts "Importing #{node_name}.zip"
+
+        invalid = false
+        file_locations = {}
+        Config.all_files.each do |file|
+          file_locations[file] = Dir.glob(File.join(dir, "#{file}*"))&.first
+          if not file_locations[file] and Config.req_files.include?(file)
+            $stderr.puts "Warning: File #{file} required in #{node_name}.zip but not found."
+            invalid = true
+          end
+        end
+
+        if invalid
+          puts "Skipping #{node_name}.zip"
+          return false
+        end
+
+        node_data = {
+          'name' => node_name,
+          'schema' => SCHEMA_NUM,
+          'type' => 'server',
+          'mutable' => {},
+          # extract data from lshw
+          'lshw' => XmlHasher.parse(File.read(file_locations['lshw-xml'])),
+          # extract data from lsblk
+          'lsblk' => LsblkParser.new(file_locations['lsblk-a-P']).hashify(),
+        }
+
+        if file_locations['groups']
+          node_data['mutable'] = YAML.load(File.read(file_locations['groups']))
+        end
+
+        Utils.exit_unless_dir(Config.yaml_dir)
+        yaml_out_name = "#{node_name}.yaml"
+        out_file = File.join(Config.yaml_dir, yaml_out_name)
+
+        if Utils.check_file_readable?(out_file)
+          old_node = Node.new(out_file)
+          old_data = old_node.open
+          # NB: this prioritses 'node_data' - new values will override old ones
+          node_data = merge_recursively(old_data, node_data)
+        end
+
+        node = Node.new(out_file)
+        node.data = node_data
+        node.save
+
+        puts "#{node_name}.zip imported to #{File.expand_path(out_file)}"
+      end
+
+      #TODO test this method with distinct & deep hashes
+      def merge_recursively(a, b)
+        a.merge(b) do |key, a_item, b_item|
+          if a_item.is_a?(Hash) and b_item.is_a?(Hash)
+            merge_recursively(a_item, b_item)
+          else
+            b_item
+          end
         end
       end
     end
