1a136761f2154edf1a84e6564280ae1276b9bbfc
stuts <stu.j.franks@gmail.com>
2020-08-27 12:18:45 +0100

    Move speed calcs out of template and into helper

--- helpers/network_device.rb
+++ helpers/network_device.rb
@@ -28,6 +28,14 @@ def network_devices
   def create_net(net_hash)
     OpenStruct.new(net_hash).tap do |o|
       o.speed = format_bits_value((net_hash['capacity'] || net_hash['size'] || 0).to_i)
+
+      # Set speed for Virtual & Infiniband interfaces
+      if o.speed == '0 bit/s' and o.logicalname&.match? /ib/
+        o.speed = 'Infiniband'
+      elsif o.speed == '0 bit/s'
+        o.speed = 'Virtual'
+      end
+
       # Handle logicalname for virtio devices (as information is buried in 'node' hash)
       if o.logicalname.nil?
         o.logicalname = o.node['logicalname']
